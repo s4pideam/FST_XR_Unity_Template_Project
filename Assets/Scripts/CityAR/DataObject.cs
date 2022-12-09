cab132f87c29f90295b36c74d90a32fc6bf6ce24
Lucas Kreber <kreberl@uni-trier.de>
2022-12-09 14:27:36 +0100

    project updated

---
+++ Assets/Scripts/CityAR/DataObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [Serializable]
+    public class DataObject
+    {
+        public Entry project;
+
+        public Entry Project
+        {
+            get => project;
+            set => project = value;
+        }
+    }
+
+    public class Entry
+    {
+        public List<Entry> files;
+        public int numberOfLines;
+        public int numberOfMethods;
+        public int numberOfAbstractClasses;
+        public int numberOfInterfaces;
+        public string name;
+        public string type;
+        public float x;
+        public float z;
+        public float w;
+        public float h;
+        public int deepth;
+        public GridObjectCollection goc;
+        public Entry parentEntry;
+        public Color color;
+    }
+}
---
+++ Assets/Scripts/CityAR/VisualizationCreator.cs
@@ -0,0 +1,141 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace CityAR
+{
+    public class VisualizationCreator : MonoBehaviour
+    {
+        private DataObject _dataObject;
+        private GameObject _platform;
+        private Data _data;
+
+        private void Start()
+        {
+            _platform = GameObject.Find("Platform");
+            _data = _platform.GetComponent<Data>();
+            _dataObject = _data.ParseData();
+            BuildCity(_dataObject);
+        }
+
+        private void BuildCity(DataObject p)
+        {
+            if (p.project.files.Count > 0)
+            {
+                p.project.w = 1;
+                p.project.h = 1;
+                p.project.deepth = 1;
+                BuildDistrict(p.project, false);
+            }
+        }
+
+        /*
+         * entry: Single entry from the data set. This can be either a folder or a single file.
+         * splitHorizontal: Specifies whether the subsequent children should be split horizontally or vertically along the parent
+         */
+        private void BuildDistrict(Entry entry, bool splitHorizontal)
+        {
+            if (entry.type.Equals("File"))
+            {
+                //TODO if entry is from type File, create building
+            }
+            else
+            {
+                float x = entry.x;
+                float z = entry.z;
+
+                float dirLocs = entry.numberOfLines;
+                entry.color = GetColorForDepth(entry.deepth);
+
+                BuildDistrictCube(entry, false);
+
+                foreach (Entry subEntry in entry.files) {
+                    subEntry.x = x;
+                    subEntry.z = z;
+                    
+                    if (subEntry.type.Equals("Dir"))
+                    {
+                        float ratio = subEntry.numberOfLines / dirLocs;
+                        subEntry.deepth = entry.deepth + 1;
+
+                        if (splitHorizontal) {
+                            subEntry.w = ratio * entry.w; // split along horizontal axis
+                            subEntry.h = entry.h;
+                            x += subEntry.w;
+                        } else {
+                            subEntry.w = entry.w;
+                            subEntry.h = ratio * entry.h; // split along vertical axis
+                            z += subEntry.h;
+                        }
+                    }
+                    else
+                    {
+                        subEntry.parentEntry = entry;
+                    }
+                    BuildDistrict(subEntry, !splitHorizontal);
+                }
+
+                if (!splitHorizontal)
+                {
+                    entry.x = x;
+                    entry.z = z;
+                    if (ContainsDirs(entry))
+                    {
+                        entry.h = 1f - z;
+                    }
+                    entry.deepth += 1;
+                    BuildDistrictCube(entry, true);
+                }
+                else
+                {
+                    entry.x = -x;
+                    entry.z = z;
+                    if (ContainsDirs(entry))
+                    {
+                        entry.w = 1f - x;
+                    }
+                    entry.deepth += 1;
+                    BuildDistrictCube(entry, true);
+                }
+            }
+        }
+
+        /*
+         * entry: Single entry from the data set. This can be either a folder or a single file.
+         * isBase: If true, the entry has no further subfolders. Buildings must be placed on top of the entry
+         */
+        private void BuildDistrictCube(Entry entry, bool isBase)
+        {
+            if (entry == null)
+            {
+                return;
+            }
+            
+            float w = entry.w; // w -> x coordinate
+            float h = entry.h; // h -> z coordinate
+            
+            if (w * h > 0)
+            {
+                //TODO create district objects
+            }
+        }
+
+        private bool ContainsDirs(Entry entry)
+        {
+            foreach (Entry e in entry.files)
+            {
+                if (e.type.Equals("Dir"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        private Color GetColorForDepth(int depth)
+        {
+            //TODO generate a color for a given depth
+            return Color.green;
+        }
+    }
+}
